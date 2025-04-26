6776476ec3107ba2b39dcb89966616c741e6f1c0
edenarielguzmanandia <edenarielguzmanandia@gmail.com>
2025-04-26 11:47:01 -0400

    Adding missing files

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa85b7762de82d1754ff1cb98ae70fd18d241f0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff24f420b795068d6eba691c93e964a50e4e152b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
